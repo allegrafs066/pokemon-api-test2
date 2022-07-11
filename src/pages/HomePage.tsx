f21064f7aee329d59745d68431c7102ba1217f9e
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-10 23:11:15 -0400

    App With filtering and Pagination

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const HomePage = () => {
+
+
+    
+
+
+  return (
+    <div className="mt-5">
+      <h1>Pokémon List</h1>
+      <hr />
+
+      <table className="table">
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+
+      </table>
+    </div>
+  );
+};
